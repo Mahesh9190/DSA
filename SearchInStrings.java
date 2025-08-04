828d3b94462d2ed0fa281f15ac27fa3103db171d
Mahesh9190 <maheshpawar7119@gmail.com>
2025-08-04 20:15:03 +0530

    Added LinearSearchInString

---
+++ SearchInStrings.java
@@ -0,0 +1,24 @@
+class SearchInStrings
+{
+    public static void main(String[] args)
+    {
+      String str="mahesh";
+      char target='m';
+      System.out.println(search(str,target));
+    }
+    public static boolean search(String str,char target)
+    {
+        if(str.length()==0)
+        {
+            return false;
+        }
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)==target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
